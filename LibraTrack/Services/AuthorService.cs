daa99ba47f9827e043dd4beedaae631bdbb7107d
Nadiia Kurtian <nadiiakurtian@gmail.com>
2025-03-20 22:11:40 +0200

    updated authorservice

--- LibraTrack/Services/AuthorService.cs
+++ LibraTrack/Services/AuthorService.cs
@@ -1,6 +1,19 @@
-﻿namespace LibraTrack.Services
+﻿using LibraTrack.Models;
+
+namespace LibraTrack.Services
 {
     public class AuthorService
     {
+        private readonly List<Author> _authors = new List<Author>();
+        public List<Author> GetAllAuthors() => _authors;
+        public Author? GetAuthorById(int id) => _authors.FirstOrDefault(x => x.Id == id);
+        public void AddAuthor(Author author) => _authors.Add(author);
+        public bool RemoveAuthor(int id)
+        {
+            var author = GetAuthorById(id);
+            if (author == null) return false;
+            _authors.Remove(author);
+            return true;
+        }
     }
 }
