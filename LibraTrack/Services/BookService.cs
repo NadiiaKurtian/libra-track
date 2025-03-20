0c14f48dd37c7483a82b86f7332c91c3e32b08e3
Nadiia Kurtian <nadiiakurtian@gmail.com>
2025-03-20 22:41:42 +0200

    updated bookservice

--- LibraTrack/Services/BookService.cs
+++ LibraTrack/Services/BookService.cs
@@ -1,17 +1,41 @@
-﻿using LibraTrack.Models;
+﻿using BookApi.Models;
+using LibraTrack.DTOs;
+using LibraTrack.Models;
 
-namespace LibraTrack.Services
+namespace BookApi.Services
 {
     public class BookService
     {
-        private readonly List<Book> _books = new List<Book>();
+        private readonly List<Book> _books = new();
+        private int _nextId = 1;
+
         public List<Book> GetAllBooks() => _books;
-        public Book? GetBookById(int id) => _books.FirstOrDefault(x => x.Id == id);
-        public void AddBok(Book book) => _books.Add(book);
-        public bool Remove(int id)
+
+        public Book GetBookById(int id) => _books.FirstOrDefault(b => b.Id == id);
+
+        public Book AddBook(Book book)
+        {
+            book.Id = _nextId++;
+            _books.Add(book);
+            return book;
+        }
+
+        public bool UpdateBook(int id, BookDto bookDto)
         {
             var book = GetBookById(id);
             if (book == null) return false;
+
+            book.Title = bookDto.Title;
+            book.Year = bookDto.Year;
+            book.AuthorId = bookDto.AuthorId;
+            return true;
+        }
+
+        public bool DeleteBook(int id)
+        {
+            var book = GetBookById(id);
+            if (book == null) return false;
+
             _books.Remove(book);
             return true;
         }
