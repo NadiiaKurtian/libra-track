e142a7da27e04dcfe7675938519757e43e2ef7e4
Nadiia Kurtian <nadiiakurtian@gmail.com>
2025-03-20 22:24:34 +0200

    updated bookservice

--- LibraTrack/Services/BookService.cs
+++ LibraTrack/Services/BookService.cs
@@ -1,6 +1,19 @@
-﻿namespace LibraTrack.Services
+﻿using LibraTrack.Models;
+
+namespace LibraTrack.Services
 {
     public class BookService
     {
+        private readonly List<Book> _books = new List<Book>();
+        public List<Book> GetAllBooks() => _books;
+        public Book? GetBookById(int id) => _books.FirstOrDefault(x => x.Id == id);
+        public void AddBok(Book book) => _books.Add(book);
+        public bool Remove(int id)
+        {
+            var book = GetBookById(id);
+            if (book == null) return false;
+            _books.Remove(book);
+            return true;
+        }
     }
 }
